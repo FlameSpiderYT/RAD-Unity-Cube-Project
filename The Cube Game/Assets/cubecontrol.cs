f588f8b1e52c4391328dcec4604345d84660b9e7
STUDENT Ethan A. Mc Cord <Ethan.A.McCord@students.ittralee.ie>
2022-10-12 12:52:21 +0100

    asset

--- The Cube Game/Assets/cubecontrol.cs
+++ The Cube Game/Assets/cubecontrol.cs
@@ -5,44 +5,52 @@ using UnityEngine;
 public class cubecontrol : MonoBehaviour
 {
     private float turningSpeed = 180;
-
+    public Transform cubeTemplate;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-        transform.position += transform.up* Time.deltaTime;
+            transform.position += transform.up * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.S))
-        transform.position -= transform.up * Time.deltaTime;
+            transform.position -= transform.up * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
-        transform.position -= transform.right * Time.deltaTime;
+            transform.position -= transform.right * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.D))
-        transform.position += transform.right * Time.deltaTime;
+            transform.position += transform.right * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Q))
-        transform.position += transform.forward * Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Z))
-        transform.position -= transform.forward * Time.deltaTime;
+            transform.position -= transform.forward * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.LeftArrow))
-        transform.Rotate(transform.up,-turningSpeed* Time.deltaTime);
+            transform.Rotate(transform.up, -turningSpeed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.RightArrow))
-        transform.Rotate(transform.up,turningSpeed* Time.deltaTime);
+            transform.Rotate(transform.up, turningSpeed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.UpArrow))
-        transform.Rotate(transform.right,-turningSpeed* Time.deltaTime);
+            transform.Rotate(transform.right, -turningSpeed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.DownArrow))
-        transform.Rotate(transform.right, turningSpeed* Time.deltaTime);
+            transform.Rotate(transform.right, turningSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(cubeTemplate, transform.position - transform.forward,
+                Quaternion.identity);
+
+        }
+
     }
 }
